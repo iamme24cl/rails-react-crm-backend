b87a84f2ef05c1fe41c86d1f04d39f097f90bbfd
Chandra Lama <iamme24cl@gmail.com>
2021-11-28 11:47:23 -0800

    refactored importing csv data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,22 @@
 require('csv')
 
+class CreateFromCSV
+
+  def self.create_prospect_and_companies(row)
+    prospect = Prospect.create(first_name: row["first_name"], last_name: row["last_name"], email: row["email"], stage: row["stage"].downcase)
+    company = Company.find_or_create_by(name: row["company"].downcase) if row["company"]
+    prospect.company = company if company
+    prospect.save
+    prospect
+  end
+
+end
 
 filename  =  File.join Rails.root, 'db/data/crm_data.csv'
 counter = 0
 
-CSV.foreach(filename, headers: true) do |row| 
-  prospect = Prospect.create(first_name: row["first_name"], last_name: row["last_name"], email: row["email"], stage: row["stage"].downcase)
-  company = Company.find_or_create_by(name: row["company"].downcase) if row["company"]
-  prospect.company = company if company
-  prospect.save
+CSV.foreach(filename, headers: true) do |row|
+  prospect = CreateFromCSV.create_prospect_and_companies(row)
   puts "#{row['email']} - #{prospect.errors.full_messages.join(",")}" if prospect.errors.any?
   counter += 1 if prospect.persisted?
 end
