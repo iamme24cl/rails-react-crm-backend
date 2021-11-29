38bab27a8b9d58e98fac463dd7f57c3304a323b2
Chandra Lama <iamme24cl@gmail.com>
2021-11-28 16:04:29 -0800

    defined update action for prospects and implemented some helper methods to find company and prospect, tested all CRUD action data

--- app/controllers/api/prospects_controller.rb
+++ app/controllers/api/prospects_controller.rb
@@ -1,4 +1,7 @@
 class Api::ProspectsController < ApplicationController
+  before_action :find_prospect, only: [:show, :update, :destroy]
+  before_action :find_company, only: [:create, :update]
+
   def index
     @prospects = Prospect.all.order created_at: :desc
     render json: @prospects
@@ -9,27 +12,33 @@ class Api::ProspectsController < ApplicationController
   end
 
   def create
-    if prospect_params[:company_id] == ""
-      company = Company.find_or_create_by(name: prospect_params[:company_name])
-    else
-      company = Company.find(prospect_params[:company_id])
-    end
-    
     prospect = Prospect.new(
       first_name: prospect_params[:first_name],
       last_name: prospect_params[:last_name],
       email: prospect_params[:email]
     )
-    prospect.company = company if company
+    prospect.company = @company if @company
 
     if prospect.save
       render json: prospect, status: :accepted
     else
-      render json: { errors: prospect.errors.full_messages }, status: :unprocessible_entity 
+      render json: { errors: prospect.errors.full_messages }, status: :unprocessible_entity
     end
   end
 
   def update
+    @prospect.update(
+      first_name: prospect_params[:first_name],
+      last_name: prospect_params[:last_name],
+      email: prospect_params[:email]
+    )
+    @prospect.company = @company if @company
+
+    if @prospect.save
+      render json: @prospect, status: :accepted
+    else
+      render json: { errors: @prospect.errors.full_messages }, status: :unprocessible_entity
+    end
   end
 
   def destroy
@@ -38,10 +47,19 @@ class Api::ProspectsController < ApplicationController
   private
 
   def prospect_params
-    params.require(:prospect).permit(:first_name, :last_name, :email, :phone, :company_id, :company_name)
+    params.require(:prospect).permit(
+      :first_name, :last_name, :email, :phone, :company_id, :company_name)
   end
 
   def find_prospect
     @prospect = Prospect.find_by(id: params[:id])
+  end
+
+  def find_company
+    if prospect_params[:company_id] == ""
+      @company = Company.find_or_create_by(name: prospect_params[:company_name])
+    else
+      @company = Company.find(prospect_params[:company_id])
+    end
   end
 end
