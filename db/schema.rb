b62d1efa9d9b5506358fc9d303806e8ed25602da
Chandra Lama <iamme24cl@gmail.com>
2021-11-28 20:57:33 -0800

    added gem bcrypt for authentication and created User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_28_170854) do
+ActiveRecord::Schema.define(version: 2021_11_29_045250) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,13 @@ ActiveRecord::Schema.define(version: 2021_11_28_170854) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "stage", default: "lead"
     t.index ["company_id"], name: "index_prospects_on_company_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
