a9a56ab1a694d853288f542ce464a7981c9db25b
chandra <iamme24cl@gmail.com>
2021-11-29 11:59:32 -0800

    generated sessions controller for authentication routes and defined the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  namespace :api do 
+  namespace :api do
+    get '/login' => 'sessions#check_if_logged_in'
+    post '/login' => 'sessions#login'
+    delete '/login/:id' => 'sessions#destroy'
+    
     resources :prospects
     resources :companies, only: [:index, :create, :update, :destroy]
   end
 
-  root 'api/prospects#index'
+  root 'api/sessions#check_if_logged_in'
 end
