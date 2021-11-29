c8c6ca2b676e805b795ba18f9caf26541f7157ac
Chandra Lama <iamme24cl@gmail.com>
2021-11-28 16:21:20 -0800

    added stage to ProspectSerilizer attribute

--- app/serializers/prospect_serializer.rb
+++ app/serializers/prospect_serializer.rb
@@ -1,4 +1,4 @@
 class ProspectSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :email, :phone, :probability
-  belongs_to :company 
+  attributes :id, :first_name, :last_name, :email, :phone, :probability, :stage
+  belongs_to :company
 end
