078347318e2e6b1dc310616fa86e340ec73a7076
chandra <iamme24cl@gmail.com>
2021-11-29 12:10:09 -0800

    generated Sessions controller and defined actions to check login status, post login and logout

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,34 @@
+class Api::SessionsController < ApplicationController
+  include SessionAuthenticatable
+
+  # Require session key authentication                                             
+  prepend_before_action :authenticate_with_session_key!, only: %i[check_if_logged_in destroy] 
+
+  def check_if_logged_in
+    user = User.find_by(id: current_bearer.id)
+    if user
+      redirect_to api_prospects_path
+    else
+      render json: { message: 'Please log in!'}, status: 403
+    end
+  end
+
+  def login
+    user = User.find_by(email: params[:email])
+    if user&.authenticate(params[:password])
+      session = user.sessions.create! token: SecureRandom.hex
+      if session.save
+        render json: session, status: 201
+      else
+        render json: { error: "Invalid Credentials!" }, status: 401
+      end
+    end
+  end
+
+  def destroy
+    session = current_bearer.sessions.find(params[:id])
+
+    session.destroy
+    render json: { message: "Successfully logged out"}, status: 200
+  end  
+end
