3232d533624618d11a9262a778f7904d2f865cb9
Chandra Lama <iamme24cl@gmail.com>
2021-11-28 19:02:44 -0800

    added routes to companies

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do 
     resources :prospects
+    resources :companies, only: [:index, :create, :update, :destroy]
   end
+
+  root 'api/prospects#index'
 end
