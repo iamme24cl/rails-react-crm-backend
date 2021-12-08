ac20c387d0c2956f960a047225c42f2bd7c12923
chandra <iamme24cl@gmail.com>
2021-12-08 14:10:13 -0800

    added before action on sessions controller to authenticate with session key, changed check_if_logged_in to render json

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,13 +1,13 @@
 class Api::SessionsController < ApplicationController
   include SessionAuthenticatable
 
-  # Require session key authentication                                             
-  prepend_before_action :authenticate_with_session_key!, only: %i[check_if_logged_in destroy] 
+  # Require session key authentication
+  prepend_before_action :authenticate_with_session_key!, only: %i[check_if_logged_in destroy]
 
   def check_if_logged_in
     user = User.find_by(id: current_bearer.id)
     if user
-      redirect_to api_prospects_path
+      render json: user, status: 200
     else
       render json: { message: 'Please log in!'}, status: 403
     end
@@ -17,11 +17,9 @@ class Api::SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user&.authenticate(params[:password])
       session = user.sessions.create! token: SecureRandom.hex
-      if session.save
-        render json: session, status: 201
-      else
-        render json: { error: "Invalid Credentials!" }, status: 401
-      end
+      render json: session, status: 201
+    else
+      render json: { error: "Invalid Credentials. Check your email and password!" }, status: 401
     end
   end
 
@@ -30,5 +28,5 @@ class Api::SessionsController < ApplicationController
 
     session.destroy
     render json: { message: "Successfully logged out"}, status: 200
-  end  
+  end
 end
