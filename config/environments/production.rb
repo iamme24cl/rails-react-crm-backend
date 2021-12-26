c9cb434d7e0a946a2f75feada2db74ed647b9da0
Chandra Lama <iamme24cl@gmail.com>
2021-12-26 11:50:28 -0800

    .

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,7 +53,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "vc_labs_crm_api_production"
+  # config.active_job.queue_name_prefix = "rails_crm_backend_production"
 
   config.action_mailer.perform_caching = false
 
